524ff0047f32d4364a59915b0a23ca4546a474d2
uangguan <312514768@qq.com>
2020-04-17 12:25:58 +0800

    增加商品管理，商品分类管理

--- src/api/productCatagory.js
+++ src/api/productCatagory.js
@@ -1,5 +1,13 @@
 import request from '@/utils/request'
 
+export function getCatagorys(params) {
+  return request({
+    url: 'api/productCatagory',
+    method: 'get',
+    params
+  })
+}
+
 export function add(data) {
   return request({
     url: 'api/productCatagory',
@@ -24,4 +32,4 @@ export function edit(data) {
   })
 }
 
-export default { add, edit, del }
+export default { add, edit, del, getCatagorys }
--- src/views/product/catagory/index.vue
+++ src/views/product/catagory/index.vue
@@ -1,69 +1,98 @@
 <template>
   <div class="app-container">
-    <!--工具栏-->
-    <div class="head-container">
-      <div v-if="crud.props.searchToggle">
-        <!-- 搜索 -->
-        <el-input v-model="query.value" clearable placeholder="输入搜索内容" style="width: 200px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
-        <el-select v-model="query.type" clearable placeholder="类型" class="filter-item" style="width: 130px">
-          <el-option v-for="item in queryTypeOptions" :key="item.key" :label="item.display_name" :value="item.key" />
-        </el-select>
-        <rrOperation :crud="crud" />
-      </div>
-      <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
-      <crudOperation :permission="permission" />
-      <!--表单组件-->
-      <el-dialog :close-on-click-modal="false" :before-close="crud.cancelCU" :visible.sync="crud.status.cu > 0" :title="crud.status.title" width="500px">
-        <el-form ref="form" :model="form" :rules="rules" size="small" label-width="80px">
-          <el-form-item label="分类名称" prop="name">
-            <el-input v-model="form.name" style="width: 370px;" />
-          </el-form-item>
-          <el-form-item label="上级分类" prop="pid">
-            未设置字典，请手动设置 Select
-          </el-form-item>
-          <el-form-item label="状态" prop="enabled">
-            <el-radio v-for="item in dict.product_catagory_status" :key="item.id" v-model="form.enabled" :label="item.value">{{ item.label }}</el-radio>
-          </el-form-item>
-          <el-form-item label="创建日期">
-            <el-input v-model="form.createTime" style="width: 370px;" />
-          </el-form-item>
-          <el-form-item label="所属商家" prop="merchantId">
-            未设置字典，请手动设置 Select
-          </el-form-item>
-        </el-form>
-        <div slot="footer" class="dialog-footer">
-          <el-button type="text" @click="crud.cancelCU">取消</el-button>
-          <el-button :loading="crud.cu === 2" type="primary" @click="crud.submitCU">确认</el-button>
+    <el-row :gutter="20">
+      <!--侧边部门数据-->
+      <el-col :xs="9" :sm="6" :md="4" :lg="4" :xl="4">
+        <div class="head-container">
+          <el-input
+            v-model="deptName"
+            clearable
+            size="small"
+            placeholder="输入部门名称搜索"
+            prefix-icon="el-icon-search"
+            class="filter-item"
+            @input="getDeptDatas"
+          />
         </div>
-      </el-dialog>
-      <!--表格渲染-->
-      <el-table ref="table" v-loading="crud.loading" :data="crud.data" size="small" style="width: 100%;" @selection-change="crud.selectionChangeHandler">
-        <el-table-column type="selection" width="55" />
-        <el-table-column v-if="columns.visible('name')" prop="name" label="分类名称" />
-        <el-table-column v-if="columns.visible('pid')" prop="pid" label="上级分类" />
-        <el-table-column v-if="columns.visible('enabled')" prop="enabled" label="状态">
-          <template slot-scope="scope">
-            {{ dict.label.product_catagory_status[scope.row.enabled] }}
-          </template>
-        </el-table-column>
-        <el-table-column v-if="columns.visible('createTime')" prop="createTime" label="创建日期">
-          <template slot-scope="scope">
-            <span>{{ parseTime(scope.row.createTime) }}</span>
-          </template>
-        </el-table-column>
-        <el-table-column v-if="columns.visible('merchantId')" prop="merchantId" label="所属商家" />
-        <el-table-column v-permission="['admin','productCatagory:edit','productCatagory:del']" label="操作" width="150px" align="center">
-          <template slot-scope="scope">
-            <udOperation
-              :data="scope.row"
-              :permission="permission"
-            />
-          </template>
-        </el-table-column>
-      </el-table>
-      <!--分页组件-->
-      <pagination />
-    </div>
+        <el-tree
+          :data="deptDatas"
+          :props="defaultProps"
+          :expand-on-click-node="false"
+          default-expand-all
+          @node-click="handleNodeClick"
+        />
+      </el-col>
+      <!--用户数据-->
+      <el-col :xs="15" :sm="18" :md="20" :lg="20" :xl="20">
+        <!--工具栏-->
+        <div class="head-container">
+          <div v-if="crud.props.searchToggle">
+            <!-- 搜索 -->
+            <el-input v-model="query.value" clearable placeholder="输入搜索内容" style="width: 200px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
+            <el-select v-model="query.type" clearable placeholder="类型" class="filter-item" style="width: 130px">
+              <el-option v-for="item in queryTypeOptions" :key="item.key" :label="item.display_name" :value="item.key" />
+            </el-select>
+            <rrOperation :crud="crud" />
+          </div>
+          <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
+          <crudOperation :permission="permission" />
+          <!--表单组件-->
+          <el-dialog :close-on-click-modal="false" :before-close="crud.cancelCU" :visible.sync="crud.status.cu > 0" :title="crud.status.title" width="500px">
+            <el-form ref="form" :model="form" :rules="rules" size="small" label-width="80px">
+              <el-form-item label="分类名称" prop="name">
+                <el-input v-model="form.name" style="width: 370px;" />
+              </el-form-item>
+              <el-form-item v-if="form.pid !== 0" style="margin-bottom: 0;" label="上级分类" prop="pid">
+                <treeselect v-model="form.pid" :options="catagorys" style="width: 178px" placeholder="选择上级类目" />
+              </el-form-item>
+              <el-form-item label="状态" prop="enabled">
+                <el-radio v-for="item in dict.product_catagory_status" :key="item.id" v-model="form.enabled" :label="item.value">{{ item.label }}</el-radio>
+              </el-form-item>
+              <el-form-item label="所属商家" prop="dept.id">
+                <treeselect
+                  v-model="form.dept.id"
+                  :options="depts"
+                  style="width: 178px"
+                  placeholder="选择所属商家"
+                  @select="selectFun"
+                />
+              </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+              <el-button type="text" @click="crud.cancelCU">取消</el-button>
+              <el-button :loading="crud.cu === 2" type="primary" @click="crud.submitCU">确认</el-button>
+            </div>
+          </el-dialog>
+          <!--表格渲染-->
+          <el-table ref="table" v-loading="crud.loading" :data="crud.data" size="small" style="width: 100%;" @selection-change="crud.selectionChangeHandler">
+            <el-table-column type="selection" width="55" />
+            <el-table-column v-if="columns.visible('name')" prop="name" label="分类名称" />
+            <el-table-column v-if="columns.visible('pid')" prop="pid" label="上级分类" />
+            <el-table-column v-if="columns.visible('enabled')" prop="enabled" label="状态">
+              <template slot-scope="scope">
+                {{ dict.label.product_catagory_status[scope.row.enabled] }}
+              </template>
+            </el-table-column>
+            <el-table-column v-if="columns.visible('createTime')" prop="createTime" label="创建日期">
+              <template slot-scope="scope">
+                <span>{{ parseTime(scope.row.createTime) }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column v-if="columns.visible('merchantId')" prop="merchantId" label="所属商家" />
+            <el-table-column v-permission="['admin','productCatagory:edit','productCatagory:del']" label="操作" width="150px" align="center">
+              <template slot-scope="scope">
+                <udOperation
+                  :data="scope.row"
+                  :permission="permission"
+                />
+              </template>
+            </el-table-column>
+          </el-table>
+          <!--分页组件-->
+          <pagination />
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -74,17 +103,30 @@ import rrOperation from '@crud/RR.operation'
 import crudOperation from '@crud/CRUD.operation'
 import udOperation from '@crud/UD.operation'
 import pagination from '@crud/Pagination'
+import { getDepts } from '@/api/system/dept'
+import Treeselect from '@riophae/vue-treeselect'
+import { getCatagorys } from '@/api/productCatagory'
 
 // crud交由presenter持有
 const defaultCrud = CRUD({ title: '商品分类', url: 'api/productCatagory', sort: 'id,desc', crudMethod: { ...crudProductCatagory }})
-const defaultForm = { id: null, name: null, pid: null, enabled: null, createTime: null, merchantId: null }
+const defaultForm = { id: null,
+  name: null,
+  pid: null,
+  enabled: null,
+  createTime: null,
+  dept: {
+    id: 1
+  }
+}
 export default {
   name: 'ProductCatagory',
-  components: { pagination, crudOperation, rrOperation, udOperation },
+  components: { Treeselect, pagination, crudOperation, rrOperation, udOperation },
   mixins: [presenter(defaultCrud), header(), form(defaultForm), crud()],
   dicts: ['product_catagory_status'],
   data() {
     return {
+      deptName: '', depts: [], deptDatas: [], jobs: [], level: 3, roles: [], catagorys: [],
+      defaultProps: { children: 'children', label: 'name' },
       permission: {
         add: ['admin', 'productCatagory:add'],
         edit: ['admin', 'productCatagory:edit'],
@@ -113,7 +155,56 @@ export default {
       ]
     }
   },
+  created() {
+    this.$nextTick(() => {
+      this.getDeptDatas()
+      // this.crud.toQuery()
+      // this.crud.msg.add = '新增成功，默认密码：123456'
+    })
+  },
   methods: {
+    // 新增与编辑前做的操作
+    [CRUD.HOOK.afterToCU](crud, form) {
+      this.getDepts()
+      this.getCatagorys()
+      // this.getRoleLevel()
+      form.enabled = form.enabled.toString()
+    },
+    // 获取左侧部门数据
+    getDeptDatas() {
+      const sort = 'id,desc'
+      const params = { sort: sort }
+      if (this.deptName) { params['name'] = this.deptName }
+      getDepts(params).then(res => {
+        this.deptDatas = res.content
+      })
+    },
+    // 获取弹窗内部门数据
+    getDepts() {
+      getDepts({ enabled: true }).then(res => {
+        this.depts = res.content
+      })
+    },
+    // 获取弹窗内分类数据
+    getCatagorys() {
+      getCatagorys({ enabled: true }).then(res => {
+        this.catagorys = res.content
+      })
+    },
+    // 切换部门
+    handleNodeClick(data) {
+      if (data.pid === 0) {
+        this.query.deptId = null
+      } else {
+        this.query.deptId = data.id
+      }
+      this.crud.toQuery()
+    },
+    // 点击部门搜索对应的岗位
+    selectFun(node, instanceId) {
+      this.getJobs(node.id)
+      this.form.job.id = null
+    },
     // 获取数据前设置好接口地址
     [CRUD.HOOK.beforeRefresh]() {
       const query = this.query
